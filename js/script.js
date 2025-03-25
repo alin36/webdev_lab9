787e4b5cf16d0ef0c51e0c921ca6a57f5149937f
alin36 <amyli2191@gmail.com>
2025-03-25 18:43:50 -0500

    finished

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
-function init(){
+function init() {
 //add your javascrip between these two lines of code
- 
+  // let message = document.getElementById("entryinput");
+  
 
+  // This works!
+  // document.getElementById("entrybutton").onclick = function () {
+  //   alert("Amy Lin: " + message.value);
+  //   document.getElementById("textoutput").innerHTML = message.value;
+  // };
 
-
-
-
-
-
+  let button = document.getElementById("entrybutton");
+  function msg() {
+    let message = document.getElementById("entryinput");
+    alert("Amy Lin: " + message.value);
+    document.getElementById("textoutput").innerHTML = message.value;
+  };
+  button.addEventListener('click', msg);
+}
 window.addEventListener('load', init);
